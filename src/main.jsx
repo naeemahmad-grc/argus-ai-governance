4ae326bf3a0046fd5829bae018997e5c5b7ed983
naeemahmad-grc <naeemahmad021@gmail.com>
2026-08-05 19:04:28 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ArgusMVP from "./ArgusMVP.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ArgusMVP />
+  </React.StrictMode>
+);
